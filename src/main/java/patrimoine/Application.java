2f3985e57b6a9c9bbcc57ced2f51c0eabda8e63c
Droopy06 <lamootalexandre@gmail.com>
2016-03-21 21:12:59 +0100

    modify users and panier
    modify users and panier

--- src/main/java/patrimoine/Application.java
+++ src/main/java/patrimoine/Application.java
@@ -1,11 +1,19 @@
 package patrimoine;
 
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DatabaseConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
     @Override
@@ -15,5 +23,11 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        /*ApplicationContext ctx = new AnnotationConfigApplicationContext(DatabaseConfiguration.class);
+        MongoOperations mongoOperation = (MongoOperations)ctx.getBean("mongoTemplate");*/
+        /*MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+        // Now connect to your databases
+        DB db = mongoClient.getDB( "patrimoine-flst" );*/
     }
 }
---
+++ src/main/java/patrimoine/DatabaseConfiguration.java
@@ -0,0 +1,55 @@
+package patrimoine;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import static java.util.Collections.singletonList;
+
+/**
+ * Created by LAMOOT Alexandre on 17/03/2016.
+ */
+@Configuration
+@EnableMongoRepositories("patrimoine")
+public class DatabaseConfiguration extends AbstractMongoConfiguration {
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+    //@Value("${spring.data.mongodb.host}")
+    private final static String host = "127.0.0.1";
+    //@Value("${spring.data.mongodb.port}")
+    private final static Integer port = 12017;
+    //@Value("${spring.data.mongodb.username}")
+    private final static String username = "patrimoine";
+    //@Value("${spring.data.mongodb.database}")
+    private final static String database = "patrimoine-flst";
+    //@Value("${spring.data.mongodb.password}")
+    private final static String password = "museehome";
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+    @Override
+    public String getDatabaseName() {
+        return database;
+    }
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient(singletonList(new ServerAddress(host, port)),
+                singletonList(MongoCredential.createCredential(username,database, password.toCharArray())));
+    }
+}
