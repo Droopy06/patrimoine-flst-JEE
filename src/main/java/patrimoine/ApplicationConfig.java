c0fd3f7fd473e4c2eeda3f2eab1b09e6b60a16a1
Droopy06 <lamootalexandre@gmail.com>
2016-12-12 17:43:15 +0100

    bug angular import.html

--- src/main/java/patrimoine/ApplicationConfig.java
+++ src/main/java/patrimoine/ApplicationConfig.java
@@ -27,6 +27,7 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/patrimoine/PartialPages/_head").setViewName("patrimoine/PartialPages/_head::content");
         registry.addViewController("/patrimoine/PartialPages/_header").setViewName("patrimoine/PartialPages/_header::content");
         registry.addViewController("/patrimoine/PartialPages/Contents/_home").setViewName("patrimoine/PartialPages/Contents/_home::content");
+        registry.addViewController("/patrimoine/PartialPages/Contents/_import").setViewName("patrimoine/PartialPages/Contents/_import::content");
     }
     @Override
     public void configureDefaultServletHandling(
--- src/main/java/patrimoine/controllers/rest/CollectionsRestController.java
+++ src/main/java/patrimoine/controllers/rest/CollectionsRestController.java
@@ -5,35 +5,32 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import patrimoine.models.Collection;
-import patrimoine.models.Evenement;
+import patrimoine.models.Pedagogique;
 import patrimoine.services.CollectionService;
-import patrimoine.services.EvenementService;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by LAMOOT Alexandre on 05/01/16.
+ * Created by ISEN on 09/12/2016.
  */
-@org.springframework.web.bind.annotation.RestController
-@RequestMapping("/rest/patrimone")
-public class SearchRestController {
+@RestController
+@RequestMapping("/rest/patrimoine")
+public class CollectionsRestController {
 
     @Autowired
     CollectionService collectionService;
-    @Autowired
-    EvenementService evenementService;
 
-    @RequestMapping(value = "/collections/list",method = RequestMethod.GET)
+    @RequestMapping(value = "/collections",method = RequestMethod.GET)
     public List<Collection> getAllCollections(){
         return collectionService.findAllData();
     }
 
-    @RequestMapping(value = "/collection/old/save",method = RequestMethod.POST)
+    @RequestMapping(value = "/collections/save",method = RequestMethod.GET)
     public Collection saveCollection(@Valid @ModelAttribute Collection collection,
-                                           BindingResult bindingResult){
+                                       BindingResult bindingResult){
         if(!bindingResult.hasErrors()){
             try {
                 return collectionService.save(collection);
@@ -42,10 +39,5 @@ public class SearchRestController {
             }
         }
         return new Collection();
-    }
-
-    @RequestMapping(value = "/evenements",method = RequestMethod.GET)
-    public List<Evenement> getAllEvenements(){
-        return evenementService.findAllData();
     }
 }
